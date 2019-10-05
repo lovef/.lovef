1623240ba1c6c0bad7b363b5180be8797e4c1ec0
Love F <lovef.code@gmail.com>
2019-10-05 13:08:37 +0200

    Gradle stub example files

---
+++ example/app/build.gradle.kts
@@ -0,0 +1,13 @@
+// plugins {
+//     id("org.jetbrains.kotlin.jvm") version "1.3.50"
+// }
+
+// dependencies {
+//     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.50")
+
+//     testImplementation("se.lovef:kotlin-assert-utils:0.9.0")
+// }
+
+// repositories {
+//     mavenCentral()
+// }
