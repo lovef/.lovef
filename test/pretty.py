5f95b1aa7dc2101652e30fc06ff161535af37263
Love F <lovef.code@gmail.com>
2020-04-28 18:49:24 +0200

    ....

--- test/pretty.py
+++ test/pretty.py
@@ -1,5 +1,6 @@
 import lib.pretty
 import unittest
+import json
 
 class TestStringMethods(unittest.TestCase):
 
@@ -7,6 +8,31 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(lib.pretty.prettify('{"a":"b"}'),
             '{\n'
             '  "a": "b"\n'
+            '}')
+        self.assertEqual(lib.pretty.prettify('{"a":{"a":"b"}}'),
+            '{\n'
+            '  "a": {\n'
+            '    "a": "b"\n'
+            '  }\n'
+            '}')
+        json.loads('{"a": "{\\"a\\": \\"b\\"}"}')
+        self.assertEqual(lib.pretty.prettify('{"a": "{\\"a\\": \\"b\\"}"}'),
+            '{\n'
+            '  "a": "{\\"a\\": \\"b\\"}"\n'
+            '}')
+
+    def test_pretty_json_recursive_string(self):
+        self.assertEqual(lib.pretty.prettify('{"a": "{\\"a\\": \\"b\\"}"}', recursive = True),
+            '{\n'
+            '  "a": {\n'
+            '    "a": "b"\n'
+            '  }\n'
+            '}')
+
+    def test_pretty_json_recursive_string_with_error(self):
+        self.assertEqual(lib.pretty.prettify('{"a": "{a\\": \\"b\\"}"}', recursive = True),
+            '{\n'
+            '  "a": "{a\\": \\"b\\"}"\n'
             '}')
 
     def test_pretty_xml(self):
