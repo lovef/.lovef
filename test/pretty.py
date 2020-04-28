e20121510252abd239653c55555591b9075fb55d
Love F <lovef.code@gmail.com>
2020-04-28 17:51:06 +0200

    pretty test

---
+++ test/pretty.py
@@ -0,0 +1,19 @@
+import lib.pretty
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_pretty_json(self):
+        self.assertEqual(lib.pretty.prettify('{"a":"b"}'),
+            '{\n'
+            '  "a": "b"\n'
+            '}')
+
+    def test_pretty_xml(self):
+        self.assertEqual(lib.pretty.prettify('<a><b/></a>'),
+        '<a>\n'
+        '  <b />\n'
+        '</a>')
+
+if __name__ == '__main__':
+    unittest.main()
