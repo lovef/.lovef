32305b13cbec5c53659cd406291c68407971c3a4
Love F <lovef.code@gmail.com>
2020-06-26 14:14:56 +0200

    Improve __git_ps1.py

--- bin/__git_ps1.py
+++ bin/__git_ps1.py
@@ -23,15 +23,33 @@ status = subprocess.check_output(['git', 'status', '--porcelain'])
 status = status.decode()
 status = status.split('\n')
 new = 0
+added = 0
 unstaged = 0
 staged = 0
+deleted = 0
+deletedStaged = 0
+renamed = 0
 for r in status:
     if r.startswith('?'):
         new += 1
         continue
     if r.startswith('M'):
         staged += 1
+    if r.startswith('A'):
+        added += 1
+    if r.startswith("R"):
+        renamed += 1
+    if r.startswith('D'):
+        deletedStaged += 1
     if r.startswith('M', 1):
         unstaged += 1
+    if r.startswith('D', 1):
+        deleted += 1
 
-print(f" {refName} {'+' * min(staged, 3)}{'*' * min(unstaged, 3)}{'?' * min(new, 3)}", end='')
+print(f" {refName} " +
+    '+' * min(staged + added, 3) +
+    '~' * min(renamed, 3) +
+    '-' * min(deletedStaged, 3) +
+    '*' * min(unstaged + deleted, 3) +
+    '?' * min(new, 3),
+     end='')
