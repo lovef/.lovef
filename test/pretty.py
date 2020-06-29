42d40aeb59292cd572ba130147131b4deeca42d5
Love F <lovef.code@gmail.com>
2020-06-29 12:12:51 +0200

    Escape non-ascii is opt in

--- test/pretty.py
+++ test/pretty.py
@@ -4,19 +4,31 @@ import unittest
 class TestStringMethods(unittest.TestCase):
 
     def test_pretty_json(self):
-        self.assertEqual(lib.pretty.prettify('{"a":"b"}'),
+        self.assertEqual(lib.pretty.main(['{"a":"b"}']),
             '{\n'
             '  "a": "b"\n'
             '}')
 
+    def test_pretty_json_non_ascii(self):
+        self.assertEqual(lib.pretty.main(['{"a":"å"}']),
+            '{\n'
+            '  "a": "å"\n'
+            '}')
+
+    def test_pretty_json_non_ascii_escaped(self):
+        self.assertEqual(lib.pretty.main(['{"a":"å"}', '--escape']),
+            '{\n'
+            '  "a": "\\u00e5"\n'
+            '}')
+
     def test_pretty_json5(self):
-        self.assertEqual(lib.pretty.prettify('{a:"b" /* comment */}'),
+        self.assertEqual(lib.pretty.main(['{a:"b" /* comment */}']),
             '{\n'
             '  "a": "b"\n'
             '}')
 
     def test_pretty_xml(self):
-        self.assertEqual(lib.pretty.prettify('<a><b/></a>'),
+        self.assertEqual(lib.pretty.main(['<a><b/></a>']),
         '<a>\n'
         '  <b />\n'
         '</a>')
