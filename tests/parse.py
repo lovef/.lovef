8b8ff4c4a03180d34298c65510101b1a40f8bfa3
Love F <lovef.code@gmail.com>
2021-02-06 17:20:13 +0100

    Parse timestamp

--- tests/parse.py
+++ tests/parse.py
@@ -1,11 +1,21 @@
 import lovef.parse
 import unittest
 
+
 class Tests(unittest.TestCase):
 
     def test_parse_base64(self):
         self.assertEqual(lovef.parse.parse("ZXhhbXBsZQ=="), "example")
         self.assertEqual(lovef.parse.parse("w6XDpMO2"), "åäö")
+
+    def test_parse_seconds_since_epoch(self):
+        self.assertEqual(lovef.parse.parse(
+            "1612625669"), "2021-02-06 16:34:29")
+
+    def test_parse_milliseconds_since_epoch(self):
+        self.assertEqual(lovef.parse.parse("1612625669123"),
+                         "2021-02-06 16:34:29.123")
+
 
 if __name__ == '__main__':
     unittest.main()
